bbdfcd49853299cc5b40fe827ce854840199cf5b
Pablo Bustos <pablo@groupon.com>
2022-01-19 09:33:07 -0600

    home

--- app/src/app/image/image.component.ts
+++ app/src/app/image/image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from "@angular/router";
 import { Memory } from '../memory';
 
 @Component({
@@ -12,7 +13,7 @@ export class ImageComponent implements OnInit {
   memory: Memory | undefined;
   touchStart: TouchEvent | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     // First get the product id from the current route.
@@ -37,9 +38,24 @@ export class ImageComponent implements OnInit {
       this.touchStart = evt;
     }
     if (type == "end") {
-      alert(evt.changedTouches[0].screenX);
       if(this.touchStart){
-        alert(this.touchStart.touches[0].screenX);
+        var deltaX = this.touchStart.touches[0].screenX - evt.changedTouches[0].screenX;
+        var deltaY = this.touchStart.touches[0].screenY - evt.changedTouches[0].screenY;
+        if (Math.abs(deltaX) > Math.abs(deltaY)){
+          if (deltaX > 0){ // left
+            if(this.memory)
+              this.memory.url = "https://m.media-amazon.com/images/I/81o+mpJbPmL._SX679_PIbundle-6,TopRight,0,0_AA679SH20_.jpg";
+          }
+          else{ // right
+            if(this.memory)
+              this.memory.url = "https://m.media-amazon.com/images/I/81bnwwQxGGS._AC_SL1500_.jpg";
+          }
+        }
+        else {
+          if (deltaY > 0) // up
+            this.router.navigate(['/']);
+        }
+        this.touchStart = undefined;
       }
     }
   }
