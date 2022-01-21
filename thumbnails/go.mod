710967a47f6d182172c672923921ec9c9e52e41a
Pablo Bustos <pablo@groupon.com>
2022-01-21 17:51:21 -0600

    x.image.draw

--- thumbnails/go.mod
+++ thumbnails/go.mod
@@ -2,4 +2,7 @@ module thumbnails
 
 go 1.17
 
-require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+)
--- thumbnails/thumbnails.go
+++ thumbnails/thumbnails.go
@@ -1,45 +1,48 @@
-// https://gist.github.com/logrusorgru/570d64fd6a051e0441014387b89286ca
+// based on https://gist.github.com/logrusorgru/570d64fd6a051e0441014387b89286ca
 package main
 
 import (
-	"github.com/nfnt/resize"
 	"image"
 	"image/jpeg"
 	"os"
-    "strconv"
+	"strconv"
+    "golang.org/x/image/draw"
 )
 
 func read(maxXY uint, path string) image.Image {
-    f, err := os.Open(path)
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
-    original_image, _, err := image.Decode(f)
-    if err != nil {
-        panic(err)
-    }
-    return resize.Resize(120, 240, original_image, resize.Lanczos3)
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	rect := image.Rect(0, 0, img.Bounds().Max.X/2, img.Bounds().Max.Y/2)
+	dst := image.NewRGBA(rect)
+	draw.CatmullRom.Scale(dst, rect, img, img.Bounds(), draw.Over, nil)
+	return dst
 }
 
 func write(img image.Image, path string) {
-    f, err := os.Create(path)
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+	f, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    err = jpeg.Encode(f, img, nil)
-    if err != nil {
-        panic(err)
-    }
+	err = jpeg.Encode(f, img, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-    maxXY, err := strconv.ParseUint(os.Args[1], 10, 32)
-    if err != nil {
-        panic(err)
-    }
-    img := read(uint(maxXY), os.Args[2])
-    write(img, os.Args[3])
+	maxXY, err := strconv.ParseUint(os.Args[1], 10, 32)
+	if err != nil {
+		panic(err)
+	}
+	img := read(uint(maxXY), os.Args[2])
+	write(img, os.Args[3])
 }
