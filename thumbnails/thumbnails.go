0bff99643c92d76023b2fd18549d22a85737dc97
Pablo Bustos <pablo@groupon.com>
2022-01-21 18:29:23 -0600

    disintegration/imaging

--- thumbnails/thumbnails.go
+++ thumbnails/thumbnails.go
@@ -1,48 +1,21 @@
-// based on https://gist.github.com/logrusorgru/570d64fd6a051e0441014387b89286ca
+// based on https://github.com/disintegration/imaging
 package main
 
 import (
-	"image"
-	"image/jpeg"
+	"github.com/disintegration/imaging"
 	"os"
 	"strconv"
-    "golang.org/x/image/draw"
 )
 
-func read(maxXY uint, path string) image.Image {
-	f, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	img, _, err := image.Decode(f)
-	if err != nil {
-		panic(err)
-	}
-	rect := image.Rect(0, 0, img.Bounds().Max.X/2, img.Bounds().Max.Y/2)
-	dst := image.NewRGBA(rect)
-	draw.CatmullRom.Scale(dst, rect, img, img.Bounds(), draw.Over, nil)
-	return dst
-}
-
-func write(img image.Image, path string) {
-	f, err := os.Create(path)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	err = jpeg.Encode(f, img, nil)
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	maxXY, err := strconv.ParseUint(os.Args[1], 10, 32)
-	if err != nil {
-		panic(err)
-	}
-	img := read(uint(maxXY), os.Args[2])
-	write(img, os.Args[3])
+	maxX, err := strconv.ParseInt(os.Args[1], 10, 32); checkErr(err)
+	img, err := imaging.Open(os.Args[2], imaging.AutoOrientation(true)); checkErr(err)
+	img = imaging.Resize(img, int(maxX), 0, imaging.Linear)
+	err = imaging.Save(img, os.Args[3]); checkErr(err)
 }
