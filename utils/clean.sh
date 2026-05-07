f369d296e3dce0a32e7e6aeb7680bf762fa24d7a
Pablo <javacomputin@gmail.com>
2026-05-06 19:30:53 -0500

    multiple fixes

--- utils/clean.sh
+++ utils/clean.sh
@@ -3,48 +3,59 @@
 set -e
 
 WORKING_DIR="$1"
-# remove trailing slash if any
-WORKING_DIR="${WORKING_DIR%/}"
-[ ! -d "$WORKING_DIR" ] && { echo "Provided path is not a directory: $WORKING_DIR"; exit 1; }
+cd $WORKING_DIR
 
-echo "working in directory: $WORKING_DIR"
+echo "working in directory: $(pwd)"
 # confirm to proceed
 read -p "Are you sure you want to proceed? (y/n) " -n 1 -r
 echo    # move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    echo "Aborting."
+    echo "Aborting ❌"
     exit 1
 fi
 
-echo "Converting HEIC files to JPG in directory: $WORKING_DIR"
-heic_files=$(ls $WORKING_DIR/*.heic 2>/dev/null || true)
+echo "🚀 Converting HEIC files to JPG"
+heic_files=$(ls *.heic 2>/dev/null || true)
 if [ -n "$heic_files" ]; then
-    ls $WORKING_DIR/*.heic | while read filename; do
-        newname="${filename%.heic}.jpg"
-        echo "Converting $filename to $newname"
-        [ -f "$newname" ] && { echo "File $newname already exists. Aborting conversion."; exit 1; }
-        sips -s format jpeg "$filename" --out "$newname"
-        rm "$filename"
+    ls *.heic | while read heicfile; do
+        newname="${heicfile%.heic}.jpg"
+        echo "Converting $heicfile to $newname"
+        [ -f "$newname" ] && { echo "File $newname already exists. Aborting ❌"; exit 1; }
+        sips -s format jpeg "$heicfile" --out "$newname"
+        rm "$heicfile"
     done
 fi
 
-echo "Verifying file extensions in directory: $WORKING_DIR"
-unknown_files=$(ls $WORKING_DIR/* | grep -vE '\.(jpg|png|mp4)$' || true)
-[ -n "$unknown_files" ] && { echo "Unknown files found: ${unknown_files}"; exit 1; }
+echo "🚀 Renaming jpeg files to jpg"
+jpeg_files=$(ls *.jpeg 2>/dev/null || true)
+if [ -n "$jpeg_files" ]; then
+    ls *.jpeg | while read jpegfile; do
+        newname="${jpegfile%.jpeg}.jpg"
+        echo "Renaming $jpegfile to $newname"
+        [ -f "$newname" ] && { echo "File $newname already exists. Aborting ❌"; exit 1; }
+        mv "$jpegfile" "$newname"
+    done
+fi
 
-echo "Removing parentheses from files in directory: $WORKING_DIR"
-for file in $(ls $WORKING_DIR/* | egrep '\([0-9]+\).(jpg|png|mp4)'); do
-  newname=$(echo $file |  sed 's/(\(0\))/_0/g')
-  [ -f $newname ] && { "tried to rename $file, but another file exists with name: ${newname}"; exit 1; }
-  echo "Renaming $file to $newname"
-  mv -i $file $newname
+echo "🚀 Verifying file extensions"
+unknown_files=$(ls * | grep -vE '\.(jpg|png|mp4)$' || true)
+[ -n "$unknown_files" ] && { echo "Unknown files found:"; echo ${unknown_files}; echo "Aborting ❌"; exit 1; }
+
+echo "🚀 Replacing strange characters from file names including spaces"
+ls * | egrep '[^0-9a-zA-Z_\.]' | while read file; do
+  newname=$(echo "$file" |  sed 's/[^0-9a-zA-Z_\.]/_/g')
+  # Remove underscore character before the dot in the extension, example: IMG_1234_.jpg to IMG_1234.jpg
+  newname=$(echo "$newname" | sed 's/_\./\./g')
+  [ -f "$newname" ] && { echo "tried to rename $file, but another file exists with name: ${newname}"; echo "Aborting ❌"; exit 1; }
+  echo "replacing strange characters, renaming $file to $newname"
+  mv -i "$file" "$newname"
 done
 
-echo "Renaming files to match timestamp format in directory: $WORKING_DIR"
-for weird in $(ls $WORKING_DIR/* | egrep -v '[0-9]{8}_[0-9]{6}(_[0-9])?.(jpg|png|mp4)'); do
+echo "🚀 Renaming files to match timestamp format"
+ls * | egrep -v '[0-9]{8}_[0-9]{6}(_.*)?.(jpg|png|mp4)' | while read weird; do
   extension="${weird##*.}"
-  newname=$WORKING_DIR/$(stat -f "%Sm" -t "%Y%m%d_%H%M%S" $weird)"_0.${extension}"
-  [ -f $newname ] && { "tried to rename $weird, but another file exists with name: ${newname}"; exit 1; }
-  echo "Renaming $weird to $newname"
-  mv -i $weird $newname
+  newname="$(stat -f "%Sm" -t "%Y%m%d_%H%M%S" "$weird")"_0."${extension}"
+  [ -f "$newname" ] && { echo "tried to rename $weird, but another file exists with name: ${newname}"; echo "Aborting ❌"; exit 1; }
+  echo "adding date-time, renaming $weird to $newname"
+  mv -i "$weird" "$newname"
 done
